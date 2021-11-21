5ce4e13d1cd99815e1e1fe07d5f6dc0c4e25824d
kelvionald <31734063+kelvionald@users.noreply.github.com>
2021-11-21 17:49:51 +0300

    реф перенес логику отрисовки

--- Assets/Scripts/TaleManager.cs
+++ Assets/Scripts/TaleManager.cs
@@ -5,39 +5,13 @@ using UnityEngine.UI;
 
 public class TaleManager : MonoBehaviour
 {
-    public GameObject[] ImageTargets;
-    public GameObject ObjectsForScene;
-    public GameObject ContentScroll;
-    public GameObject ContentScrollItem;
-
     public GameObject CurrentTarget;
 
     void Start()
     {
-        int i = 0;
-        float imageHeight = 58;
 
-        foreach (Transform child in ObjectsForScene.transform)
-        {
-            if (null == child)
-                continue;
-
-            Texture2D img = RuntimePreviewGenerator.GenerateModelPreview(child);
-            GameObject obj = Instantiate(ContentScrollItem, ContentScroll.transform);
-            obj.transform.localPosition = changeY(obj.transform.localPosition, obj.transform.localPosition.y - i * imageHeight);
-            obj.GetComponent<RawImage>().texture = img;
-            obj.GetComponent<PreviewSceneObject>().sceneObject = child.gameObject;
-            child.gameObject.SetActive(false);
-            i++;
-        }
     }
 
-    Vector3 changeY(Vector3 position, float changeY)
-    {
-        return new Vector3(position.x, changeY, position.z);
-    }
-
-    // Update is called once per frame
     void Update()
     {
         
---
+++ Assets/Scripts/UI/DrawPreviewSceneObjects.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawPreviewSceneObjects : MonoBehaviour
+{
+    public GameObject[] ImageTargets;
+    public GameObject ObjectsForScene;
+    public GameObject ContentScroll;
+    public GameObject ContentScrollItem;
+
+    void Start()
+    {
+        int i = 0;
+        float imageHeight = 58;
+
+        foreach (Transform child in ObjectsForScene.transform)
+        {
+            if (null == child)
+                continue;
+
+            Texture2D img = RuntimePreviewGenerator.GenerateModelPreview(child);
+            GameObject obj = Instantiate(ContentScrollItem, ContentScroll.transform);
+            obj.transform.localPosition = changeY(obj.transform.localPosition, obj.transform.localPosition.y - i * imageHeight);
+            obj.GetComponent<RawImage>().texture = img;
+            obj.GetComponent<PreviewSceneObject>().sceneObject = child.gameObject;
+            child.gameObject.SetActive(false);
+            i++;
+        }
+    }
+
+    Vector3 changeY(Vector3 position, float changeY)
+    {
+        return new Vector3(position.x, changeY, position.z);
+    }
+}
