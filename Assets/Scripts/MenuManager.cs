c45e89a4e3fa798a38abdae34f4fa78525680632
kelvionald <31734063+kelvionald@users.noreply.github.com>
2021-12-12 11:32:24 +0300

    добавление коллайдера для перемещения объектов

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -88,6 +88,8 @@ public class MenuManager : MonoBehaviour
                 GameObject model = Importer.LoadFromFile(path);
                 model.transform.SetParent(ObjectsForScene.transform);
                 model.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+                model.AddComponent<BoxCollider>();
+                model.AddComponent<MoveObj>();
             }
             GetComponent<DrawPreviewSceneObjects>().RenderObjectsPreview();
         }
